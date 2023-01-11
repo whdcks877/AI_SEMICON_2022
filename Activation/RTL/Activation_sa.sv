836560478ce30bd4529645865afcda1bb4ef0efe
Suhyeok <108323423+tngurdla@users.noreply.github.com>
2023-01-11 12:13:37 +0900

    Add files via upload

---
+++ Activation/RTL/Activation_sa.sv
@@ -0,0 +1,87 @@
+//version 2022-12-29
+//editor IM SUHYEOK
+
+module Activation_sa #(
+    parameter       DATA_WIDTH      = 8,
+    parameter       ADDRESS_WIDTH   = 10
+    )
+(
+    input   wire                clk,
+    input   wire                rst,
+    
+    input   wire                acc_last_i, 
+    input   wire                acc_valid_i,
+    //output  wire                act_ready_o,  not yet used
+    input   wire                [DATA_WIDTH-1:0] acc_result_i,
+    input   wire                [ADDRESS_WIDTH-1:0] acc_result_address_i,
+
+    output                      act_last_o, 
+    output                      act_valid_o,
+    //input   wire                pool_ready_i, not yet used
+    output                      [DATA_WIDTH-1:0] act_result_o,
+    output                      [ADDRESS_WIDTH-1:0] act_result_address_o
+);
+    localparam  S_INIT  = 1'd0,
+                S_ACT   = 1'd1;
+
+    //reg                         acc_valid;
+    reg                         act_valid;
+    reg                         act_last;
+    reg                         [DATA_WIDTH-1:0] act_result;
+    reg                         [ADDRESS_WIDTH-1:0] act_result_address;
+    reg                         state, state_n;
+
+    always_ff @(posedge clk) begin
+        if(!rst) begin    
+            state <= S_INIT;
+            //act_valid <= 1'b0;
+            //act_last <= 1'b0;
+            //act_result <= 'b0;
+        end else begin
+            state <= state_n;
+        end
+    end
+
+
+     always_comb begin
+        state_n = state;
+        act_valid = 1'b0; 
+        //act_last = 1'b0; The location of this should be discussed later.
+
+        case(state)
+            S_INIT: begin
+                state_n = S_ACT;
+                //act_valid = 1'b0; The location of this should be discussed later.
+                act_last = 1'b0;
+                act_result = 'b0;
+                act_result_address = 'b0;
+            end
+            S_ACT: begin
+                if(acc_valid_i) begin
+                    act_valid = 1'b1;
+                    if (!acc_last_i) begin
+                    act_result_address = acc_result_address_i;
+                        if(!acc_result_i[DATA_WIDTH-1]) begin
+                            act_result = acc_result_i;
+                        end else begin
+                            act_result = 'b0;
+                        end
+                    end
+                    else begin
+                        act_last = 1'b1;
+                        state_n = S_INIT;
+                    end
+                end
+                else begin
+                    state_n = S_INIT;
+                end
+            end
+        endcase
+    end
+
+    assign act_last_o               = act_last;
+    assign act_valid_o              = act_valid;
+    assign act_result_o             = act_result;
+    assign act_result_address_o     = act_result_address;
+
+endmodule
---
+++ Activation/RTL/Activation_x17.sv
@@ -0,0 +1,70 @@
+module Activation_x17 #(
+    parameter       ACC_NUM             = 16,
+    parameter       FA_NUM              = 1,
+    parameter       ADDRESS_WIDTH       = 10,
+    parameter       DATA_WIDTH          = 8
+)
+(
+    input                       clk,
+    input                       rst,
+    
+    input                       acc_last_i [ACC_NUM + FA_NUM], 
+    input                       acc_valid_i [ACC_NUM + FA_NUM],
+    //output  wire                act_ready_o [ACC_NUM],
+    input                       [DATA_WIDTH-1:0] acc_result_i [ACC_NUM + FA_NUM],
+    input                       [ADDRESS_WIDTH-1:0] acc_result_address_i [ACC_NUM], //FA address not use
+
+    output                      act_last_o [ACC_NUM  + FA_NUM],
+    output                      act_valid_o [ACC_NUM  + FA_NUM],
+    //input   wire                pool_ready_i [ACC_NUM],
+    output                      [DATA_WIDTH-1:0] act_result_o [ACC_NUM  + FA_NUM],
+    output                      [ADDRESS_WIDTH-1:0] act_result_address_o [ACC_NUM]
+);
+    wire                        acc_last [ACC_NUM + FA_NUM];
+    wire                        acc_valid [ACC_NUM + FA_NUM];
+    wire                        [DATA_WIDTH-1:0] acc_result [ACC_NUM + FA_NUM];
+    wire                        [ADDRESS_WIDTH-1:0] acc_result_address [ACC_NUM];
+    wire                        act_last [ACC_NUM + FA_NUM];
+    wire                        act_valid [ACC_NUM + FA_NUM];
+    wire                        [DATA_WIDTH-1:0] act_result [ACC_NUM + FA_NUM];
+    wire                        [ADDRESS_WIDTH-1:0] act_result_address [ACC_NUM];
+    
+   genvar index;
+    generate
+        for (index=0; index<ACC_NUM; index++) begin
+            Activation_sa u_act_sa(
+                .clk(clk),
+                .rst(rst),
+                .acc_last_i(acc_last[index]),
+                .acc_valid_i(acc_valid[index]),
+                .acc_result_i(acc_result[index]),
+                .acc_result_address_i(acc_result_address[index]),
+                .act_last_o(act_last[index]),
+                .act_valid_o(act_valid[index]),
+                .act_result_o(act_result[index]),
+                .act_result_address_o(act_result_address[index])
+            );
+        end
+    endgenerate
+
+    Activation_fc u_act_fc(
+        .clk(clk),
+        .rst(rst),
+        .acc_last_i(acc_last[ACC_NUM]),
+        .acc_valid_i(acc_valid[ACC_NUM]),
+        .acc_result_i(acc_result[ACC_NUM]),
+        .act_last_o(act_last[ACC_NUM]),
+        .act_valid_o(act_valid[ACC_NUM]),
+        .act_result_o(act_result[ACC_NUM])
+    );
+
+    assign acc_last                             = acc_last_i;
+    assign acc_valid                            = acc_valid_i;
+    assign acc_result                           = acc_result_i;
+    assign acc_result_address                   = acc_result_address_i;
+    assign act_last_o                           = act_last;
+    assign act_valid_o                          = act_valid;
+    assign act_result_o                         = act_result;
+    assign act_result_address_o                 = act_result_address;
+
+endmodule
